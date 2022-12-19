7d0fccbf30f602ef9839aca3f93826ea891d46b4
Djily SARR <Djilysarr275@gmail.com>
2022-12-19 14:19:44 +0100

    Home page

---
+++ lib/UI/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 123, 8, 29),
+        foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+        title: const Text("Home"),
+      ),
+      body: Container(),
+    );
+  }
+}
