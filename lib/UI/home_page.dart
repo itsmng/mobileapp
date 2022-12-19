fd594db7edcbc2e12ba998d203f8cce724157ae6
Djily SARR <Djilysarr275@gmail.com>
2022-12-19 14:26:14 +0100

    Menu on the home page

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobileapp/UI/navigation_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      drawer: NavigationDrawer(),
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 123, 8, 29),
         foregroundColor: const Color.fromARGB(255, 255, 255, 255),
---
+++ lib/UI/navigation_drawer.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:mobileapp/UI/home_page.dart';
+
+class NavigationDrawer extends StatelessWidget {
+  NavigationDrawer({super.key});
+  // ignore: prefer_const_constructors
+  final padding = EdgeInsets.symmetric(horizontal: 20);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Material(
+        // ignore: prefer_const_constructors
+        color: Color.fromARGB(255, 123, 8, 29),
+        child: ListView(
+          children: <Widget>[
+            buildHeader(
+              urlImage: "assets/login_logo_itsm.png",
+              text: "Application techniciens ITSM-NG",
+            ),
+            Container(
+              padding: padding,
+              child: Column(
+                children: [
+                  const Divider(
+                    color: Colors.white70,
+                  ),
+                  const SizedBox(height: 14),
+                  buildMenuItem(
+                    text: 'Accueil',
+                    icon: Icons.add_to_home_screen,
+                    onClicked: () => seletedItem(context, 0),
+                  ),
+                  
+                  
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildMenuItem(
+      {required text, required IconData icon, VoidCallback? onClicked}) {
+    const color = Colors.white;
+    const hoverColor = Colors.white70;
+
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: color,
+      ),
+      title: Text(
+        text,
+        style: const TextStyle(color: color),
+      ),
+      hoverColor: hoverColor,
+      onTap: onClicked,
+    );
+  }
+
+  void seletedItem(BuildContext context, int index) {
+    Navigator.of(context).pop();
+
+    switch (index) {
+      case 0:
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => const HomePage(),
+        ));
+        break;
+     
+    }
+  }
+
+  Widget buildHeader({
+    required String urlImage,
+    required String text,
+  }) {
+    return InkWell(
+      child: Container(
+        padding: padding.add(const EdgeInsets.symmetric(vertical: 40)),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(urlImage, width: 150),
+                const SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  text,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
