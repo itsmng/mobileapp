8feb1a3f69533dc47fdf6bbce2e3df0760ce22a3
Djily SARR <Djilysarr275@gmail.com>
2023-01-02 11:51:30 +0100

    get tickets by status (without solved/closed)

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/UI/navigation_drawer.dart';
+import 'package:mobileapp/api/api_endpoints.dart';
+import 'package:mobileapp/models/tickets_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,6 +11,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late List<Tickets> futureTicket;
+  var ticket = Tickets();
+  var listTicketsData = {};
+  // ignore: prefer_typing_uninitialized_variables
+  var apiResponse;
+
+  @override
+  void initState() {
+    super.initState();
+    apiResponse = ticket.apiMgmt.get(ApiEndpoint.apiGetAllTickets);
+    handleTickets();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +33,30 @@ class _HomePageState extends State<HomePage> {
         foregroundColor: const Color.fromARGB(255, 255, 255, 255),
         title: const Text("Home"),
       ),
-      body: Container(),
     );
+  }
+
+  handleTickets() async {
+    futureTicket = await ticket.fetchTicketsData(apiResponse);
+    int cptNew = 0;
+    int cptAssigned = 0;
+    int cptPlanned = 0;
+    int cptPending = 0;
+
+    for (var element in futureTicket) {
+      if (element.status == 1) {
+        cptNew++;
+      } else if (element.status == 2) {
+        cptAssigned++;
+      } else if (element.status == 3) {
+        cptPlanned++;
+      } else if (element.status == 4) {
+        cptPending++;
+      }
+    }
+    listTicketsData["new"] = cptNew;
+    listTicketsData["processing (assigned)"] = cptAssigned;
+    listTicketsData["processing (planned)"] = cptPlanned;
+    listTicketsData["pending"] = cptPending;
   }
 }
