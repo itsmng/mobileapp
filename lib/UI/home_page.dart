1478bd89b287e35c9c935e6492d1deef8929bf33
Djily SARR <Djilysarr275@gmail.com>
2023-01-03 16:02:36 +0100

    Chart for tickets status

--- lib/UI/home_page.dart
+++ lib/UI/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mobileapp/UI/navigation_drawer.dart';
 import 'package:mobileapp/api/api_endpoints.dart';
+import 'package:mobileapp/models/bar_model.dart';
 import 'package:mobileapp/models/tickets_model.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,15 +15,57 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late List<Tickets> futureTicket;
   var ticket = Tickets();
-  var listTicketsData = {};
-  // ignore: prefer_typing_uninitialized_variables
-  var apiResponse;
+  Map listTicketsData = {};
+
+  dynamic apiResponse;
 
   @override
   void initState() {
     super.initState();
     apiResponse = ticket.apiMgmt.get(ApiEndpoint.apiGetAllTickets);
     handleTickets();
+  }
+
+  List<charts.Series<BarMmodel, String>> _createTicketsChart() {
+    List<BarMmodel> data = [];
+
+    if (listTicketsData["new"] != null &&
+        listTicketsData["processing (assigned)"] != null &&
+        listTicketsData["processing (planned)"] != null &&
+        listTicketsData["pending"] != null) {
+      data = [
+        BarMmodel(
+            "New",
+            listTicketsData["new"],
+            charts.ColorUtil.fromDartColor(
+                const Color.fromARGB(255, 78, 121, 167))),
+        BarMmodel(
+            "Assigned",
+            listTicketsData["processing (assigned)"],
+            charts.ColorUtil.fromDartColor(
+                const Color.fromARGB(255, 242, 142, 44))),
+        BarMmodel(
+            "Planned",
+            listTicketsData["processing (planned)"],
+            charts.ColorUtil.fromDartColor(
+                const Color.fromARGB(255, 225, 87, 89))),
+        BarMmodel(
+            "Pending",
+            listTicketsData["pending"],
+            charts.ColorUtil.fromDartColor(
+                const Color.fromARGB(255, 118, 183, 178))),
+      ];
+    }
+
+    return [
+      charts.Series<BarMmodel, String>(
+        data: data,
+        id: 'graphTickets',
+        colorFn: (BarMmodel barModeel, _) => barModeel.barColor,
+        domainFn: (BarMmodel barModeel, _) => barModeel.title,
+        measureFn: (BarMmodel barModeel, _) => barModeel.value,
+      )
+    ];
   }
 
   @override
@@ -33,20 +77,87 @@ class _HomePageState extends State<HomePage> {
         foregroundColor: const Color.fromARGB(255, 255, 255, 255),
         title: const Text("Home"),
       ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              padding: const EdgeInsets.all(10),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(children: <Widget>[
+                    Text(
+                      "Tickets status",
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    Expanded(
+                      child: charts.BarChart(
+                        _createTicketsChart(),
+                        animate: true,
+                      ),
+                    ),
+                  ]),
+                ),
+              ),
+            ),
+            Container(
+              height: 80,
+              padding: const EdgeInsets.all(10),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(3),
+                  
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            "Latest 24h",
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          Text(
+                            listTicketsData["latest tickets"].toString(),
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            "Late tickets",
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                          Text(
+                            listTicketsData["late tickets"].toString(),
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   handleTickets() async {
     futureTicket = await ticket.fetchTicketsData(apiResponse);
+
     int newTicket = 0;
     int assignedTicket = 0;
     int plannedTickets = 0;
     int pendingTickets = 0;
     int latestTickets = 0;
     int lateTickets = 0;
-
     DateTime currentDate = DateTime.now();
     late Duration diff;
+
     for (var element in futureTicket) {
       if (element.status == 1) {
         newTicket++;
@@ -71,12 +182,14 @@ class _HomePageState extends State<HomePage> {
         latestTickets++;
       }
 
-      listTicketsData["new"] = newTicket;
-      listTicketsData["processing (assigned)"] = assignedTicket;
-      listTicketsData["processing (planned)"] = plannedTickets;
-      listTicketsData["pending"] = pendingTickets;
-      listTicketsData["latest tickets"] = latestTickets;
-      listTicketsData["late tickets"] = lateTickets;
+      setState(() {
+        listTicketsData["new"] = newTicket;
+        listTicketsData["processing (assigned)"] = assignedTicket;
+        listTicketsData["processing (planned)"] = plannedTickets;
+        listTicketsData["pending"] = pendingTickets;
+        listTicketsData["latest tickets"] = latestTickets;
+        listTicketsData["late tickets"] = lateTickets;
+      });
     }
   }
 }
