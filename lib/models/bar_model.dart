dd38c00e39394870b32fffb653f9611fc198e6c5
Djily SARR <Djilysarr275@gmail.com>
2023-01-03 15:58:56 +0100

    Chart model for tickets status

---
+++ lib/models/bar_model.dart
@@ -0,0 +1,8 @@
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class BarMmodel {
+  final String title;
+  final int value;
+  final charts.Color barColor;
+  BarMmodel(this.title, this.value, this.barColor);
+}
