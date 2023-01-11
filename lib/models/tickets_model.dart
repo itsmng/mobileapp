824548a3470723ff91c6a89f3ad34cc72fbe916c
Djily SARR <Djilysarr275@gmail.com>
2023-01-11 10:19:43 +0100

    Add title/category/location in ticket's model

--- lib/models/tickets_model.dart
+++ lib/models/tickets_model.dart
@@ -9,6 +9,9 @@ class Tickets {
   String? date;
   int? status;
   String? timeToResolve;
+  String? title;
+  String? category;
+  String? location;
 
   final apiMgmt = ApiMgmt();
 
@@ -16,13 +19,25 @@ class Tickets {
     this.date,
     this.status,
     this.timeToResolve,
+    this.title,
+    this.category,
+    this.location,
   });
 
   factory Tickets.fromMap(Map<String, dynamic> json) {
+    if (json["itilcategories_id"] == 0) {
+      json["itilcategories_id"] = "";
+    }
+    if (json["locations_id"] == 0) {
+      json["locations_id"] = "";
+    }
     return Tickets(
       date: json["date"],
       status: json["status"],
       timeToResolve: json["time_to_resolve"],
+      title: json["name"],
+      category: json["itilcategories_id"],
+      location: json["locations_id"],
     );
   }
 
