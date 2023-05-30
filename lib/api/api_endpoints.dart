10cf406218ad762962d2ec64a36db4d86831fd11
RudyLaurent <rudy@factorfx.com>
2023-05-30 09:22:31 +0200

    refactor(mobilapp): refract api routes

--- lib/api/api_endpoints.dart
+++ lib/api/api_endpoints.dart
@@ -2,44 +2,10 @@ class ApiEndpoint {
   // Current full session
   static const String apiGetFullSession = "getFullSession/";
 
-  // Ticket API Endpoint
-  static const String apiRootTicket = "Ticket/?expand_dropdowns=true";
-  static const String apiUpdateTicket = "Ticket/";
-  static const String apiGetAllTickets =
-      "Ticket/?expand_dropdowns=true&range=0-40000";
-  static const String apiGetMyTickets =
-      "Ticket/?expand_dropdowns=true&range=0-40000";
-  static const String apiGetTicketsDetails = "Ticket/%s?expand_dropdowns=true";
-  static const String apiDeleteTicket = "Ticket/";
-
-  // Task API Endpoint
-  static const String apiRootTicketTasks = "TicketTask/";
-  static const String apiGetTicketTasks =
-      "TicketTask/?expand_dropdowns=true&searchText[tickets_id]=%s";
-
   // Followup API Endpoint
   static const String apiRootTicketFollowup = "ITILFollowup/";
   static const String apiGetTicketFollowup =
-      "ITILFollowup/?expand_dropdowns=true&searchText[items_id]=";
-
-  // Task API Endpoint
-  static const String apiRootTicketTask = "TicketTask/";
-  static const String apiGetTicketTask =
-      "TicketTask/?expand_dropdowns=true&searchText[tickets_id]=";
-
-  // Ticket user Endpoint
-  static const String apiGetTicketUsers =
-      "Ticket_User/?expand_dropdowns=true&searchText[tickets_id]=%s&searchText[type]=2";
-  static const String apiGetTicketAssigned =
-      "Ticket_User/?expand_dropdowns=false&searchText[users_id]=%s&searchText[type]=2";
-
-  // Computer API Endpoint
-  static const String apiGetAllComputers =
-      "Computer/?expand_dropdowns=true&range=0-40000";
-  static const String apiGetAllComputersByName =
-      "Computer/?expand_dropdowns=true&searchText[name]=^%s&range=0-40000";
-  static const String apiGetOneComputers = "Computer/%s?expand_dropdowns=true";
-  static const String apiRootComputer = "Computer/";
+      "ITILFollowup/?searchText[items_id]=";
 
   // Special status API Endpoint
   static const String apiGetAllSpecialStatus = "SpecialStatus";
@@ -56,17 +22,50 @@ class ApiEndpoint {
   // All User API Endpoint
   static const String apiGetAllUsers = "User";
 
-  // Ticket user API Endpoint
-  static const String apiGetAllTicketUsers =
-      "Ticket_User/?expand_dropdowns=true&searchText[type]=2";
-  static const String apiUpdateTicketUser = "Ticket_User/";
-
   // State API Endpoint
   static const String apiGetAllStateComputer = "State";
 
   // Update source API Endpoint
   static const String apiGetRootUpdateSource = "Autoupdatesystem";
 
-  // Item ticket API Endpoint
-  static const String apiRootItemTicket = "Item_Ticket/?expand_dropdowns=true";
+  // Items Ticket
+  static const String apiRootItemTicket = "Item_Ticket";
+  static const String apiGetItemTicketByComputer = 
+      "Item_Ticket?searchText[itemtype]=Computer&searchText[items_id]=";
+
+  // Ticket
+  static const String apiRootTicket = "Ticket/?expand_dropdowns=true";
+  static const String apiUpdateTicket = "Ticket/";
+  static const String apiGetTicketsById = "Ticket/";
+  static const String apiGetAllTickets =
+      "Ticket/?range=0-40000";
+  static const String apiGetMyTickets =
+      "Ticket/?range=0-40000";
+  static const String apiGetTicketsDetails = "Ticket/%s?expand_dropdowns=true";
+  static const String apiDeleteTicket = "Ticket/";
+
+  //Ticket User
+  static const String apiUpdateTicketUser = "Ticket_User/";
+  static const String apiGetAllTicketUsers =
+      "Ticket_User/?searchText[type]=2";
+  static const String apiGetTicketUsers =
+      "Ticket_User/?searchText[tickets_id]=%s&searchText[type]=2";
+  static const String apiGetTicketAssigned =
+      "Ticket_User/?searchText[users_id]=%s&searchText[type]=2";
+
+  // Computer 
+  static const String apiGetAllComputers =
+      "Computer/?range=0-40000";
+  static const String apiGetAllComputersByName =
+      "Computer/?searchText[name]=^%s&range=0-40000";
+  static const String apiGetOneComputers = "Computer/%s?expand_dropdowns=true";
+  static const String apiRootComputer = "Computer/";
+
+  // Ticket Tasks
+  static const String apiRootTicketTasks = "TicketTask/";
+  static const String apiGetTicketTasks =
+      "TicketTask/?searchText[tickets_id]=%s";
+  static const String apiRootTicketTask = "TicketTask/";
+  static const String apiGetTicketTask =
+      "TicketTask/?searchText[tickets_id]=";
 }
